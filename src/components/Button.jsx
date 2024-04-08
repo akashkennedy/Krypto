f43b459a2c0d5351112589cc389bf9bdc0804049
akash kennedy <akashkennedy1@gmail.com>
2024-04-08 11:21:49 +0530

    Create Button Component

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({label}) => {
+  return (
+    <button className="bg-[#8f5edf] rounded-full p-3 w-32 relative bottom-20 left-20 font-semibold mr-3">
+       {label}
+    </button>
+  )
+}
+
+export default Button
