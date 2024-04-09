2e669585e546f08a9f9818de3c45f00e2ce0e5ee
akash kennedy <akashkennedy1@gmail.com>
2024-04-09 18:26:23 +0530

    Add Hover Effects

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,22 +8,22 @@ const Header = () => {
       <BlurCircle className="absolute left-96 bg-[#8959d8] z-0"/>
       <nav className="mt-3">
         <ul className="flex items-center justify-between gap-5">
-          <li>
+          <li className="transition-all hover:-translate-y-0.5">
             <a href="#" className="text-lg font-medium">
               About
             </a>
           </li>
-          <li>
+          <li className="transition-all hover:-translate-y-0.5">
             <a href="#" className="text-lg font-medium">
               Pricing
             </a>
           </li>
-          <li>
+          <li className="transition-all hover:-translate-y-0.5">
             <a href="#" className="text-lg font-medium">
               Contact
             </a>
           </li>
-          <li>
+          <li className="transition-all hover:-translate-y-0.5">
             <a href="#" className="text-lg font-medium">
               Buy NFTs
             </a>
