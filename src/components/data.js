808d20cdb8ed274a64ac7aceec27af530fffcd54
akash kennedy <akashkennedy1@gmail.com>
2024-04-12 18:01:32 +0530

    Update Navlinks using map method

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+
+  const navLinks = [
+    {
+        id: 1,
+        name: "About",
+        href: "#"
+    },
+    {
+        id: 2,
+        name: "Pricing",
+        href: "#"
+    },
+    {
+        id: 3,
+        name: "Contact",
+        href: "#"
+    },
+    {
+        id: 4,
+        name: "Buy NFTs",
+        href: "#"
+    }
+]
+
+export default navLinks;
